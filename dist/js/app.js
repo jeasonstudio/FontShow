07174722212d24c43eb80941adb84a59ef9a8158
赵吉彤 <zjtong3576@sina.com>
2016-10-31 14:13:00 +0800

    0.5.1

--- dist/js/app.js
+++ dist/js/app.js
@@ -15,7 +15,11 @@ app.controller('mainCtrl', function ($scope, $rootScope, $http) {
     // var otherHeight = $(".page-header").height() + $(".form-group").height() + $(".footer").height();
     // $(".inner").height($(window).height() - otherHeight - 60)
     $("textarea").trigger("click");
-    $scope.clearAll = function() {
-        $scope.inputWords = '';
+    var clearIt = function() {$scope.inputWords = '';}
+    $scope.clearAll = function () {
+        $(".inner span").fadeOut(1000);
+        // $scope.inputWords = '';
+        setTimeout("clearIt()", 1000)
+        $(".inner span").fadeIn(10);
     }
 });
