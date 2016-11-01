84c77a26e11064c97dd94ac251ca070cb186760e
赵吉彤 <zjtong3576@sina.com>
2016-11-01 22:59:56 +0800

    s

--- src/js/app.js
+++ src/js/app.js
@@ -6,21 +6,24 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('main', {
             url: "/main",
-            templateUrl: "../../html/main.html",
+            templateUrl: "./html/main.html",
             controller: "mainCtrl"
         })
 });
 
 app.controller('mainCtrl', function ($scope, $rootScope, $http) {
-    var mainWord = $scope.inputWords;
-    $scope.clearAll = function (mainWord) {
-        $(".inner span").fadeOut(1000,function(mainWord) {
+    // 一次性清理文字
+    $scope.clearAll = function () {
+        $(".inner span").fadeOut(1000,function() {
             $(".inner span").fadeIn(10);
-            mainWord = '';
         });
+        setTimeout("$scope.inputWords = '';",2000);
     }
+    // 默认选中字体
+    $(".innerSpan").css('font-family','Bertholdr');
     $scope.changeFonts = function(fontName) {
         console.log(fontName)
         $(".innerSpan").css('font-family',fontName);
     }
+    $scope.font = 'Bertholdr';
 });
